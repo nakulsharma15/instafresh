7ffaef795653f9063c4d88f41189c44209653fc4
Nakul Sharma <88782512+nakulsharma15@users.noreply.github.com>
2022-04-07 16:58:33 +0530

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import {Banner , Categories , Footer , Header } from "../Components/index.js";
 export default function Home () {
 
 return (<>
-    <div className="flex-column min-height">
+    <div className="flex-column stick-bottom">
     <Header />
      <Banner />
      <Categories />
